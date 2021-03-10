a4664bb96a026e73193d971bcf21adb314310cd1
Josh Schiavone <jshschiavone@gmail.com>
2021-03-09 22:29:08 -0500

    added file integrity checks

--- src/bf.cc
+++ src/bf.cc
@@ -22,107 +22,114 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#include "bf.h" 
+#include "bf.h"
 
 namespace bf {
-  
+
   Environment::Environment() : program_memory(bf::max_memory_size) {
     wipe_instruction_buffer(true);
     bf_memory_pointer = program_memory.begin();
   }
 
   void Environment::wipe_instruction_buffer(bool exec) {
-    program_instruction.clear(); 
+    program_instruction.clear();
     bf_instruction_pointer = program_instruction.begin();
   }
-  
-  template <typename BFBytes, typename BFStream, typename BFType>  
+
+  bool Environment::check_program_file_extension(std::string fn) {
+    if (fn.substr(fn.find_last_of(".") + 1) == "bf") {
+      return true;
+    } else { return false; }
+  }
+
+  template <typename BFBytes, typename BFStream, typename BFType>
   bfint_t Stream<BFBytes, BFStream, BFType>::entry_point_line(Environment& env, BFBytes& linestream, bfint_t bracks) {
     istream = std::istringstream(linestream);
     return entry_point_stream(env, istream, bracks);
   }
-  
+
   template <typename BFBytes, typename BFStream, typename BFType>
   bfint_t Stream<BFBytes, BFStream, BFType>::entry_point_stream(Environment& env, BFStream& bfstream, bfint_t bracks) {
-    bf_word stream_word; 
+    bf_word stream_word;
     if (bracks == BF_SUCCESS_CODE_STANDARD) { env.program_instruction.push_back(bf::nullchar); }
     while (bf::bfuck_should_succeed<bool> (true, true)) {
-      bfstream >> stream_word; 
+      bfstream >> stream_word;
       if (!bfstream) {
-        break; 
+        break;
       }
       switch (stream_word) {
         case BF_CELL_SHIFT_BACK: env.program_instruction.push_back(stream_word); break;
         case BF_CELL_SHIFT_FRONT: env.program_instruction.push_back(stream_word); break;
         case BF_INCREMENT_VALUE: env.program_instruction.push_back(stream_word); break;
         case BF_DECREMENT_VALUE: env.program_instruction.push_back(stream_word); break;
-        case BF_READ_STDIN_BUFFER: env.program_instruction.push_back(stream_word); break; 
+        case BF_READ_STDIN_BUFFER: env.program_instruction.push_back(stream_word); break;
         case BF_SHOW_STDOUT_BUFFER: env.program_instruction.push_back(stream_word); break;
         case BF_LOOP_BEGIN:
-          bracks++; 
+          bracks++;
           env.program_instruction.push_back(stream_word);
           break;
         case BF_LOOP_END:
           bracks--;
-          env.program_instruction.push_back(stream_word); 
+          env.program_instruction.push_back(stream_word);
           break;
-        default: break; 
+        default: break;
 
       }
 
     }
     if (bracks == BF_SUCCESS_CODE_STANDARD) { env.program_instruction.push_back(bf::nullchar); }
-    return bracks; 
+    return bracks;
   }
-  
+
   template <typename BFBytes, typename BFStream, typename BFType>
   void Stream<BFBytes, BFStream, BFType>::output_bf_stream(BFType printablechar_) {
     isprint(printablechar_) ? std::cout << printablechar_
       : std::cout << std::dec << static_cast<bfint_t> (printablechar_);
   }
-  
+
   void Parser::get_loop_closer(Environment& env) {
-    temp_value = 1; 
-    do { 
-      env.bf_instruction_pointer++; 
-      if (*env.bf_instruction_pointer == '[') temp_value++; 
+    temp_value = 1;
+    do {
+      env.bf_instruction_pointer++;
+      if (*env.bf_instruction_pointer == '[') temp_value++;
       else if (*env.bf_instruction_pointer == ']') temp_value--;
-    } while (temp_value != BF_SUCCESS_CODE_STANDARD); 
+    } while (temp_value != BF_SUCCESS_CODE_STANDARD);
   }
 
+
   void Parser::get_loop_opener(Environment& env) {
-    temp_value = BF_SUCCESS_CODE_STANDARD;  
-    do { 
-      if (*env.bf_instruction_pointer == '[') temp_value++; 
-      else if (*env.bf_instruction_pointer == ']') temp_value--; 
-      env.bf_instruction_pointer--; 
+    temp_value = BF_SUCCESS_CODE_STANDARD;
+    do {
+      if (*env.bf_instruction_pointer == '[') temp_value++;
+      else if (*env.bf_instruction_pointer == ']') temp_value--;
+      env.bf_instruction_pointer--;
     } while (temp_value != BF_SUCCESS_CODE_STANDARD);
   }
 
   void Parser::interpret_bf_instructions(Environment& env) {
-    env.bf_instruction_pointer = env.program_instruction.begin(); 
+    env.bf_instruction_pointer = env.program_instruction.begin();
     while (env.bf_instruction_pointer != env.program_instruction.end()) {
       switch (*env.bf_instruction_pointer) {
-        case BF_INCREMENT_VALUE: (*env.bf_memory_pointer)++; env.bf_instruction_pointer++; break; 
+        case BF_INCREMENT_VALUE: (*env.bf_memory_pointer)++; env.bf_instruction_pointer++; break;
         case BF_DECREMENT_VALUE: (*env.bf_memory_pointer)--; env.bf_instruction_pointer++; break;
-        case BF_CELL_SHIFT_FRONT: 
+        case BF_CELL_SHIFT_FRONT:
                 if (env.bf_memory_pointer != (env.program_memory.end()--)) {
                   env.bf_memory_pointer++;
                 }
-                env.bf_instruction_pointer++; 
+                env.bf_instruction_pointer++;
                 break;
         case BF_CELL_SHIFT_BACK:
                 if (env.bf_memory_pointer != env.program_memory.begin()) {
                    env.bf_memory_pointer--;
-                } 
-                env.bf_instruction_pointer++; 
-                break; 
+                }
+                env.bf_instruction_pointer++;
+                break;
         case BF_SHOW_STDOUT_BUFFER: output_bf_stream(*env.bf_memory_pointer); env.bf_instruction_pointer++; break;
-        case BF_READ_STDIN_BUFFER: 
-                bf_word input_word; std::cin >> input_word; 
-                (*env.bf_memory_pointer) = input_word; 
-                env.bf_instruction_pointer++; 
-                break; 
+        case BF_READ_STDIN_BUFFER:
+                bf_word input_word; std::cin >> input_word;
+                (*env.bf_memory_pointer) = input_word;
+                env.bf_instruction_pointer++;
+                break;
         case BF_LOOP_BEGIN:
                 if (!(*env.bf_memory_pointer))
                   get_loop_closer(env);
@@ -130,8 +137,8 @@ namespace bf {
                 break;
         case BF_LOOP_END:
                get_loop_opener(env); env.bf_instruction_pointer++; break;
-        case bf::nullchar: env.bf_instruction_pointer++; break;  
-      }     
+        case bf::nullchar: env.bf_instruction_pointer++; break;
+      }
 
     }
   }
@@ -142,46 +149,50 @@ namespace bf {
       if (!std::cin) {
         throw "(error): broken input stream\n";
         break;
-      } 
+      }
       if (temp_bracks == BF_SUCCESS_CODE_STANDARD) { env.wipe_instruction_buffer(true);  }
       BF_SET_VALUE(temp_bracks, entry_point_line(env, interpreter_prompt, temp_bracks));
       if (temp_bracks == BF_SUCCESS_CODE_STANDARD) {
-        interpret_bf_instructions(env); 
-        output_bf_stream(*env.bf_memory_pointer); 
-        BF_FLUSH(std::endl); 
+        interpret_bf_instructions(env);
+        output_bf_stream(*env.bf_memory_pointer);
+        BF_FLUSH(std::endl);
       } else {
           std::cout << "...>>>";
       }
     }
-    return true;  
+    return true;
   }
 
-  
+
 } // namespace: bf
 
 int main(int argc, char** argv) {
 
   bf::Environment env;
-  bf::Stream<std::string, std::istream, bf_word> bf_stream; 
-  bf::Parser parser; 
+  bf::Stream<std::string, std::istream, bf_word> bf_stream;
+  bf::Parser parser;
 
   if (argc == BF_SUCCESS_CODE_STANDARD + 1) {
     bf::User usr;
-    std::cout << "The Brainfuck Interactive Console Interpreter\n\t\tJosh Schiavone\n"; 
+    std::cout << "The Brainfuck Interactive Console Interpreter\n\t\tJosh Schiavone\n";
     usr.load_interactive_interpreter(env);
   } else {
-      std::ifstream stream = std::ifstream(argv[1]); 
-      if (stream.is_open()) {
-        bfint_t bracks = bf_stream.entry_point_stream(env, stream);
-        if (bracks == BF_SUCCESS_CODE_STANDARD) {
-          parser.interpret_bf_instructions(env); 
+      if (env.check_program_file_extension(argv[1])) {
+        std::ifstream stream = std::ifstream(argv[1]);
+        if (stream.is_open()) {
+          bfint_t bracks = bf_stream.entry_point_stream(env, stream);
+          if (bracks == BF_SUCCESS_CODE_STANDARD) {
+            parser.interpret_bf_instructions(env);
+          } else {
+              throw bf::exp::BFException("(error): could not match brackets\n");
+          }
         } else {
-            throw bf::exp::BFException("(error): could not match brackets\n");
-        } 
+            throw bf::exp::BFException("(error): .bf source could not be located\n");
+        }
       } else {
-          throw bf::exp::BFException("(error): .bf source could not be located\n");
+          throw bf::exp::BFException("(error): source file must contain a valid brainfuck extension\n");
       }
   }
 
-  return 0; 
+  return 0;
 }
--- src/bf.h
+++ src/bf.h
@@ -33,11 +33,11 @@ SOFTWARE.
 #include <algorithm>
 #include <ctype.h>
 
-#define BF_SUCCESS_CODE_STANDARD 0 
-#define BF_ERROR_CODE_STANDARD -1 
+#define BF_SUCCESS_CODE_STANDARD 0
+#define BF_ERROR_CODE_STANDARD -1
 
 #define BF_SET_VALUE(s_value, k_value) ((s_value) = (k_value))
-#define BF_FLUSH(ENDSTREAM) (std::cout << ENDSTREAM) 
+#define BF_FLUSH(ENDSTREAM) (std::cout << ENDSTREAM)
 
 /*
  *Brainfuck instructions
@@ -50,7 +50,7 @@ SOFTWARE.
 #define BF_READ_STDIN_BUFFER ','
 #define BF_SHOW_STDOUT_BUFFER '.'
 #define BF_LOOP_BEGIN '['
-#define BF_LOOP_END ']' 
+#define BF_LOOP_END ']'
 #define BF_RESET '\0'
 
 /*
@@ -68,72 +68,71 @@ SOFTWARE.
 #define OPCODE_LOOP_END 8
 #define OPCODE_RESET 9
 
-
-typedef char bf_word; 
-typedef uint32_t bf32_int; 
+typedef char bf_word;
+typedef uint32_t bf32_int;
 typedef int bfint_t;
 
 namespace bf {
 
-  constexpr bf_word nullchar = '\0'; 
+  constexpr bf_word nullchar = '\0';
   const bfint_t max_memory_size = 30000; // tape
 
   template <typename RType>
   RType bfuck_should_succeed(RType successor, RType successor_value) {
     return (std::move(successor) == successor_value);
-  } 
+  }
 
   class Environment {
   public:
-    std::vector<bf_word> program_memory, program_instruction; 
-    std::vector<bf_word>::iterator bf_instruction_pointer, bf_memory_pointer; 
+    std::vector<bf_word> program_memory, program_instruction;
+    std::vector<bf_word>::iterator bf_instruction_pointer, bf_memory_pointer;
     explicit Environment();
 
     void wipe_instruction_buffer(bool exec);
-  }; 
+    bool check_program_file_extension(std::string fn);
+  };
 
   template <typename BFBytes, typename BFStream, typename BFType>
   class Stream : public Environment {
   public:
-    bfint_t entry_point_line(Environment& env, BFBytes& linestream, bfint_t bracks=0); 
+    bfint_t entry_point_line(Environment& env, BFBytes& linestream, bfint_t bracks=0);
     bfint_t entry_point_stream(Environment& env, BFStream& bfstream, bfint_t bracks=0);
   protected:
     void output_bf_stream(BFType printablechar_);
   private:
-    std::istringstream istream; 
+    std::istringstream istream;
   };
 
   class Parser : protected Stream<std::string, std::istream, bf_word> {
   public:
-    void interpret_bf_instructions(Environment& env); 
+    void interpret_bf_instructions(Environment& env);
   private:
-    bfint_t temp_value; 
+    bfint_t temp_value;
   private:
-    void get_loop_closer(Environment& env); 
-    void get_loop_opener(Environment& env);  
+    void get_loop_closer(Environment& env);
+    void get_loop_opener(Environment& env);
   };
 
   class User : protected Parser {
   public:
-    bool load_interactive_interpreter(Environment& env); 
+    bool load_interactive_interpreter(Environment& env);
   private:
     bfint_t temp_bracks = BF_SUCCESS_CODE_STANDARD;
-    std::string interpreter_prompt; 
+    std::string interpreter_prompt;
   };
 
   namespace exp {
     #include <exception>
     class BFException : public std::exception {
     public:
-      BFException(const char* const message): errmessage_ {message} {}; 
+      BFException(const char* const message): errmessage_ {message} {};
 
       const char* what() const noexcept { return errmessage_; }
-    private: 
-      const char* errmessage_;  
+    private:
+      const char* errmessage_;
     };
 
-  } //namespace: exp 
+  } //namespace: exp
 
 
 }
-
